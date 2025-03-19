58defb336ba2c316b1cd2336c161404e47826dd7
zapr12yl <syapril2024@gmail.com>
2025-03-19 18:45:29 +0700

    Update setup.py

--- migration/setup.py
+++ migration/setup.py
@@ -6,14 +6,13 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
-        "psycopg2-binary>=2.9.6",
+        "psycopg2-binary>=2.9.10",
         "python_tds>=1.16.0",
-        "sqlglot>=10.6.2",
-        "html.parser>=0.2",
-        "python-dotenv>=0.19.2",
+        "sqlglot>=26.11.1",
+        "python-dotenv>=1.0.1",
         "migrator>=0.0.8", 
-        "sqlalchemy>=2.0.38",
+        "sqlalchemy>=2.0.39",
         "typing-extensions>=4.12.2"
     ],
-    python_requires=">=3.12",
+    python_requires=">=3.13",
 )
